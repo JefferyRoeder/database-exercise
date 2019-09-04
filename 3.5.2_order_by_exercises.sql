981696f25d4933ff7fa05dda38c121aaeb9774ef
JefferyRoeder <jefferyroeder@gmail.com>
2019-09-04 11:40:17 -0500

    updated 3.5.2 exercise with question #7

--- 3.5.2_order_by_exercises.sql
+++ 3.5.2_order_by_exercises.sql
@@ -12,14 +12,16 @@ SELECT * FROM employees WHERE first_name IN('Irena','Vidya','Maya') ORDER BY fir
 SELECT * FROM employees WHERE first_name IN('Irena','Vidya','Maya') ORDER BY last_name, first_name;
 
 -- #5
-SELECT * FROM employees WHERE last_name LIKE 'E%' ORDER BY emp_no;
+SELECT * FROM employees WHERE last_name LIKE '%E%' ORDER BY emp_no;
 
 -- #6
 SELECT * FROM employees WHERE first_name IN('Irena','Vidya','Maya') ORDER BY last_name DESC, first_name DESC;
 
-SELECT * FROM employees WHERE last_name LIKE 'E%' ORDER BY emp_no DESC;
+SELECT * FROM employees WHERE last_name LIKE '%E%' ORDER BY emp_no DESC;
 
+-- #7
 
+SELECT * FROM employees WHERE hire_date BETWEEN '1990-1-1' AND '1999-12-31' AND birth_date LIKE '%12-25' ORDER BY birth_date ASC, hire_date DESC;
 
 
 
