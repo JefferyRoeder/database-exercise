2ce25c6e79ed4c31b04064c5f6553a3c076d3cc2
JefferyRoeder <jefferyroeder@gmail.com>
2019-09-09 14:56:16 -0500

    3.11 more exercises. Finished required questions, bonus pending

--- 3.11_more_exercises.sql
+++ 3.11_more_exercises.sql
@@ -1,36 +1,100 @@
--- #3.11_more_exercises.sql
+-- 3.11_more_exercises --
 
--- #1 How much do the current managers of each department get paid, relative to the average salary for the department? Is there any department where the department manager gets paid less than the average salary?
-use employees;
 
--- # selects most current salary per employee
-SELECT emp_no, salary, to_date 
+/* #1How much do the current managers of each department get paid, relative to the average salary for the department? Is there any department where the department manager gets paid less than the average salary? Customer service and Production managers make less than their average dept salary */
+
+SELECT * FROM salaries;
+
+CREATE TEMPORARY TABLE bayes_810.salary_temp AS
+SELECT S.emp_no, S.salary, DE.dept_no, D.dept_name
 FROM salaries S
-WHERE to_date=(SELECT MAX(to_date) FROM salaries WHERE emp_no = S.emp_no) and  ;
+JOIN dept_emp AS DE ON DE.emp_no = S.emp_no
+JOIN departments as D ON D.dept_no = DE.dept_no
+WHERE S.to_date = '9999-01-01';
+
+
+SELECT * from bayes_810.salary_temp;
+
+SELECT D.dept_name, round(avg(ST.salary),0) as avg_salary
+FROM departments D
+JOIN bayes_810.salary_temp as ST ON ST.dept_no = D.dept_no
+GROUP BY D.dept_name;
+
+
+SELECT D.dept_name, S.salary as manager_salary, SQ.avg_salary, SQ.avg_salary - S.salary as diff_sal
+FROM dept_manager DM
+LEFT JOIN salaries AS S ON S.emp_no = DM.emp_no
+JOIN departments as D ON D.dept_no = DM.dept_no
+JOIN(
+	SELECT DSUB.dept_name, round(avg(ST.salary),0) as avg_salary
+	FROM departments DSUB
+	JOIN bayes_810.salary_temp as ST ON ST.dept_no = DSUB.dept_no
+	GROUP BY DSUB.dept_name) as SQ ON SQ.dept_name = D.dept_name
+
+WHERE S.to_date = '9999-01-01' AND DM.to_date = '9999-01-01'
+ORDER BY D.dept_name;
 
 
 
-USE employees;
-SELECT * FROM titles LIMIT 10;
-SELECT * from salaries LIMIT 10;
-SELECT * FROM dept_emp;
-SELECT * FROM dept_manager;
+-- # 2.1 What languages are spoken in Santa Monica?
 
-SELECT titles.emp_no, titles.title, salaries.salary, salaries.emp_no as sal_emp_no FROM salaries
-LEFT JOIN titles on salaries.emp_no = titles.emp_no WHERE title = 'Manager';
+USE world;
+
+SELECT * FROM city;
+SELECT * FROm country;
+SELECT * FROM countrylanguage;
+
+SELECT CL.language, CL.percentage
+FROM countrylanguage as CL
+
+JOIN city C on C.countrycode = CL.countrycode
+WHERE name = 'Santa Monica' AND CL.countrycode = 'USA'
+ORDER BY CL.percentage;
 
 
--- # selects most recent salary for all managers
-SELECT titles.emp_no, titles.title, S.salary, S.emp_no, dept_manager.dept_no
-FROM salaries S 
-LEFT JOIN dept_manager on S.emp_no = dept_manager.emp_no
-LEFT JOIN titles on S.emp_no = titles.emp_no
-WHERE S.to_date=(SELECT Max(salaries.to_date) FROM salaries WHERE emp_no = S.emp_no) AND title = 'Manager';
+-- #2.2 How many different countries are in each region?
 
--- # joins emp_no and dept_emp to grab avg salary by dept
-SELECT dept_emp.dept_no, avg(S.salary)
-FROM salaries S
-LEFT JOIN dept_emp on S.emp_no = dept_emp.emp_no
-GROUP BY dept_emp.dept_no
-;
 
+SELECT region, count(code) as num_countries
+FROM country
+GROUP BY region
+ORDER BY count(code);
+
+
+-- #2.3 What is the population for each region?
+
+SELECT region, sum(Population) as population
+FROM country
+GROUP BY region
+ORDER BY sum(population) DESC;
+
+
+-- #2.4 Population for each continent
+
+SELECT continent, sum(Population) as population
+FROM country
+GROUP BY continent
+ORDER BY sum(population) DESC;
+
+
+-- #2.5 What is the average life expectancy globally?
+
+
+SELECT avg(lifeexpectancy)
+FROM country;
+
+-- #2.6 What is the average life expectancy for each region, each continent? Sort the results from shortest to longest
+
+SELECT continent, avg(lifeexpectancy) as life_expectancy
+FROM country
+GROUP BY continent
+ORDER BY life_expectancy;
+
+
+SELECT region, avg(lifeexpectancy) as life_expectancy
+FROM country
+GROUP BY region
+ORDER BY life_expectancy;
+
+
+-- BONUS #1
